07961502beb6c9dd0cd4da340d5b8d6d74067c0d
qzhou <313410158@qq.com>
2018-09-20 19:12:53 +0800

    fix

--- machine_learning/pd.py
+++ machine_learning/pd.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
-s = pd.Series([1,3,6,np.nan,44,1])
 
-print(s)
+def build_q_table(n_states, actions):
+    table = pd.DataFrame(
+        np.zeros((n_states, len(actions))),     # q_table initial values
+        columns=actions,    # actions's name
+    )
+    # print(table)    # show table
+    return table
+
+print(build_q_table(6,["L","R"]))
+
